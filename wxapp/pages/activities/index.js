bf83c206786b344322f1b2f3f3e3b2da047a9fdc
richard <1042651820@qq.com>
2018-01-29 22:32:15 +0800

    完善活动主页

--- wxapp/pages/activities/index.js
+++ wxapp/pages/activities/index.js
@@ -11,8 +11,56 @@ Page({
       "../../assets/slidepicture/2.jpg",
       "../../assets/slidepicture/3.jpg"
     ],
+    location: {
+      latitude: 0,
+      longitude: 0,
+      address: "中山大学"
+    },
+    locationIconUrl: "../../assets/icon/location.png",
+    searchKey: "",
   },
   onLoad: function() {
+    this.getLocation();
+  },
+
+  getLocation: function() {
+    var that = this
+    var locationString = ""
+    wx.getLocation({
+      type: 'wgs84',
+      success: function(res) {
+        that.setData({
+          'location.latitude': res.latitude,
+          'location.longitude': res.longitude
+        })
+        locationString = that.data.location.latitude + "," + that.data.location.longitude
+        wx.request({
+          url: "https://apis.map.qq.com/ws/geocoder/v1/",
+          data: {
+            "key": "6UVBZ-LUKLU-3ZRV7-BUDEO-SZJCZ-CQBN4",
+            "location": locationString
+          },
+          methon: "GET",
+          success: function(res) {
+            that.setData({
+              'location.address': res.data.result.address
+            })
+            console.log("请求成功")
+            console.log("请求地址为：" + res.data.result.address)
+          },
+          fail: function(res) {
+            console.log("定位失败")
+          }
+        })
+      }
+    })
+
+
+  },
+  getSearchInput: function(e) {
+    this.setData({
+      searchKey: e.detail.value
+    })
   }
 
 })
--- wxapp/pages/index/index.js
+++ wxapp/pages/index/index.js
@@ -1,7 +1,19 @@
 Page({
   data:{
-
+    userInfo: {}
   },
+  onLoad: function() {
+    var that = this
+    wx.getUserInfo({
+      success: function(res) {
+        that.setData({
+          userInfo: res.userInfo
+        })
+        console.log("success")
+      }
+    })
+  },
+
   entry: function() {
     wx.switchTab({
       url:"../activities/index"
