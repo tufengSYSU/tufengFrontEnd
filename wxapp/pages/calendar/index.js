4e99c175d8bcab33ee35f05d0a6969562a5b4103
isanbel <chroslen@gmail.com>
2018-01-31 21:33:28 +0800

    delete the test scripts in the calender page

--- wxapp/pages/calendar/index.js
+++ wxapp/pages/calendar/index.js
@@ -1,27 +1,3 @@
 Page({
-  data: {
-    hotList: [
-      {
-        pic: '../../assets/articlepicture/1.png',
-        title: '玻璃棧道',
-        desc: '22W人去過'
-      }, {
-        pic: '../../assets/articlepicture/1.png',
-        title: '玻璃棧道',
-        desc: '22W人去過'
-      }, {
-        pic: '../../assets/articlepicture/1.png',
-        title: '玻璃棧道',
-        desc: '22W人去過'
-      }, {
-        pic: '../../assets/articlepicture/1.png',
-        title: '玻璃棧道',
-        desc: '22W人去過'
-      }, {
-        pic: '../../assets/articlepicture/1.png',
-        title: '玻璃棧道',
-        desc: '22W人去過'
-      }
-    ]
-  }
+  data: {}
 })
