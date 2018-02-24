c206697d45a3b5d3b90f251bdb43f903b5a40f1b
richard <1042651820@qq.com>
2018-02-24 21:16:06 +0800

    完善社团主页 完成相册部分

--- wxapp/pages/myprofile/organization/index.js
+++ wxapp/pages/myprofile/organization/index.js
@@ -19,17 +19,28 @@ Page({
         name: "2016维纳斯歌友会",
         imgUrl: "../../../assets/articlepicture/2.png",
         progress: false
-      }
+      },
+      {
+        name: "张剑见面会",
+        imgUrl: "../../../assets/articlepicture/3.png",
+        progress: false
+      },
+      {
+        name: "唱飞自我 最帅张园园",
+        imgUrl: "../../../assets/articlepicture/4.png",
+        progress: true
+      },
+      {
+        name: "图蜂最牛逼 楼上说得对",
+        imgUrl: "../../../assets/slidepicture/3.jpg",
+        progress: true
+      },
     ],
     picture: [
       "../../../assets/articlepicture/1.png",
       "../../../assets/articlepicture/2.png",
       "../../../assets/articlepicture/3.png",
       "../../../assets/articlepicture/4.png",
-      "../../../assets/articlepicture/4.png",
-      "../../../assets/articlepicture/4.png",
-      "../../../assets/articlepicture/1.png",
-      "../../../assets/articlepicture/2.png",
       "../../../assets/album/1.jpg",
       "../../../assets/album/2.jpg",
       "../../../assets/album/3.jpg",
@@ -37,11 +48,21 @@ Page({
       "../../../assets/album/5.jpg"
     ],
     hoverclass: [
-      "non-hover-text",
+      "hover-text",
       "non-hover-text"
     ],
     activityOrAlbum: true,
-    attention: false
+    attention: false,
+    coverImageCss: [
+      "cross-cover-image",
+      "vertical-cover-image"
+    ],
+    introductionCss: [
+      "cross-activity-introduction",
+      "vertical-activity-introduction"
+    ],
+    test: false,
+    test_css: "display: none"
   },
 
   onLoad: function() {
@@ -52,9 +73,13 @@ Page({
     this.data.hoverclass[0] = "non-hover-text";
     this.data.hoverclass[1] = "non-hover-text";
     this.data.hoverclass[index] = "hover-text";
+    this.data.test = (index == 1 ? true : false);
+    this.data.test_css = (index == 1 ? "" : "display: none");
     this.setData({
       hoverclass: this.data.hoverclass,
-      activityOrAlbum: !this.data.activityOrAlbum
+      activityOrAlbum: (index == 0 ? true : false),
+      test: this.data.test,
+      test_css: this.data.test_css
     })
   },
   makeAttention: function(obj) {
