d1c12f31fa608404db6017617d6c97144da3498c
unknown <1042651820@qq.com>
2018-04-29 15:03:39 +0800

    主页 推送卡片 活动卡片

--- wxapp/pages/activities/index.js
+++ wxapp/pages/activities/index.js
@@ -3,6 +3,7 @@
  * @author bravos
  */
 
+//  TODO: 抓取推送日期并加到推送卡片上
 const app = getApp()
 const tools = require('./tools.js')
 const ASSETS = "../../assets/"
@@ -13,342 +14,332 @@ const BALL_ICON = ASSETS + "icon/mark/ball.png"
 const MONEY_ICON = ASSETS + "icon/mark/money.png"
 
 Page({
-  data:{
-    userInfo: undefined,
-    windowWidth: 0,
-    searchKey: "",
-    // 轮播图片的路径 使用每天的第一篇推送来做
-    // 每篇推送是否已阅览
-    visited: [],
-    subTab: ["推送+", "我看过", "公益", "文娱"],
-    currentTab: 2
-  },
-  /**
-   * 初始化数据
-   */
-  onLoad: function() {
-    // 两个请求
-    this.getLocation();
-    this.getIcons();
-    this.getWindowSize();
-    this.setDateFormat();
-    this.getArticlesInOneMonth();
-    this.getFunction();
-  },
-  swiperChange: function(e) {
-    console.log(e.detail.current)
-  },
-  getFunction: function() {
-    this.formatUrl = tools.formatUrl;
-    this.parseHTML = tools.parseHTML;
-  },
-  getIcons: function() {
-    this.setData({
-      locationIcon: LOCATION_ICON,
-      searchIcon: SEARCH_ICON,
-      aixinIcon: AIXIN_ICON,
-      ballIcon: BALL_ICON,
-      moneyIcon: MONEY_ICON
-    })
-    this.setData({
-      defaultTitle: "中大图峰",
-      defaultImage: "https://i.loli.net/2018/04/17/5ad5f1e0c85cc.png"
-    })
-  },
-  getWindowSize: function() {
-    var that = this
-    wx.getSystemInfo({
-      success: function(res) {
-        let windowSize = {
-          width: res.windowWidth,
-          height: res.windowHeight
-        }
-        that.setData({
-          windowSize
+    data: {
+        userInfo: undefined,
+        windowWidth: 0,
+        searchKey: "",
+        // 轮播图片的路径 使用每天的第一篇推送来做
+        // 每篇推送是否已阅览
+        visited: [],
+        subTab: ["推送+", "我看过", "公益", "文娱"],
+        currentTab: 0,
+        currentDate: "4月29日"
+    },
+    /**
+     * 初始化数据
+     */
+    onLoad: function() {
+        // 两个请求
+        this.getLocation();
+        this.getIcons();
+        this.getWindowSize();
+        this.setDateFormat();
+        this.getArticlesInOneMonth();
+        this.getFunction();
+    },
+    swiperChange: function(e) {
+        console.log(e.detail.current)
+    },
+    getFunction: function() {
+        this.formatUrl = tools.formatUrl;
+        this.parseHTML = tools.parseHTML;
+    },
+    getIcons: function() {
+        this.setData({
+            locationIcon: LOCATION_ICON,
+            searchIcon: SEARCH_ICON,
+            aixinIcon: AIXIN_ICON,
+            ballIcon: BALL_ICON,
+            moneyIcon: MONEY_ICON
         })
-      }
-    })
-  },
-  getArticlesInOneMonth: function() {
-    let date = new Date().format();
-    var that = this;
-    //console.log(date)
-    wx.request({
-      url: "https://ancestree.site/api/activity_stages",
-      data: {
-        one_day_in_that_month: date
-      },
-      success: function(res) {
-        let stages = res.data.data
-        that.setData({
-          stages
+        this.setData({
+            defaultTitle: "中大图峰",
+            defaultImage: "https://i.loli.net/2018/04/17/5ad5f1e0c85cc.png"
         })
-        that.getPhoto();
-      }
-    })
-  },
-  getPhoto: function() {
-    var that = this;
-    var stages = this.data.stages;
-    console.log(stages)
-    var count = 0;
-    var article = stages.map((stagesInOneDay, index) => {
-      var articlesInOneDay = stagesInOneDay.map((stage, index) => {
-        //console.log(count)
-        let url = that.formatUrl(stage.activity.wechat_url);
-        //console.log(stage.id + " " + stage.activity.wechat_url + " " + url)
+    },
+    getWindowSize: function() {
+        var that = this
+        wx.getSystemInfo({
+            success: function(res) {
+                let windowSize = {
+                    width: res.windowWidth,
+                    height: res.windowHeight
+                }
+                that.setData({
+                    windowSize
+                })
+            }
+        })
+    },
+    getArticlesInOneMonth: function() {
+        let date = new Date().format();
+        var that = this;
+        //console.log(date)
         wx.request({
-          url: url,
-          success: function(res) {
-            let data = res.data
-            stage.title = that.parseHTML(data, "msg_title");
-            stage.image = that.parseHTML(data, "msg_cdn_url");
-            if (stage.title === that.data.defaultTitle) {
-              console.log(stage.id + " ")
+            url: "https://ancestree.site/api/activity_stages",
+            data: {
+                one_day_in_that_month: date
+            },
+            success: function(res) {
+                let stages = res.data.data
+                that.setData({
+                    articlesInOneMonth: stages
+                })
+                that.getPhoto();
             }
-            that.setData({
-              stages
+        })
+    },
+    getPhoto: function() {
+        var that = this;
+        var articlesInOneMonth = this.data.articlesInOneMonth;
+        var count = 0;
+        var article = articlesInOneMonth.map((articlesInOneDay, index) => {
+            articlesInOneDay.map((article, index) => {
+                //console.log(count)
+                let url = that.formatUrl(article.activity.wechat_url);
+                //console.log(stage.id + " " + stage.activity.wechat_url + " " + url)
+                wx.request({
+                    url: url,
+                    success: function(res) {
+                        let data = res.data
+                        article.title = that.parseHTML(data, "msg_title");
+                        article.image = that.parseHTML(data, "msg_cdn_url");
+                        article.publish_time = that.parseHTML(data, "publish_time").slice(0, 10);
+                        if (article.title === that.data.defaultTitle) {
+                            console.log(article.id + " ")
+                        }
+                        that.setData({
+                            articlesInOneMonth
+                        })
+                    },
+                    fail: function(res) {
+                        article.title = that.data.defaultTitle
+                        article.image = that.data.defaultImage
+                        that.setData({
+                            articlesInOneMonth
+                        })
+                    },
+                    complete: function() {
+                        count++;
+                        //console.log(count)
+                        if (count === 42) {
+                            that.setDateToActivities();
+                            that.setData({
+                                articlesInOneMonth: that.data.articlesInOneMonth.reverse()
+                            })
+                            that.getPostUrl();
+                            that.formatDateForarticles();
+                        }
+                        //console.log(that.data.stages)
+                    }
+                })
             })
-          },
-          fail: function(res) {
-            stage.title = that.data.defaultTitle
-            stage.image = that.data.defaultImage
-            that.setData({
-              stages
+            return articlesInOneDay;
+        })
+    },
+    // 跳转到活动主页
+    getPostUrl: function() {
+        let articlesInOneMonth = this.data.articlesInOneMonth;
+        let posters = []
+        for (let i in articlesInOneMonth) {
+            let articlesInOneDay = articlesInOneMonth[i].articlesInOneDay
+            articlesInOneDay.forEach((article) => {
+                let item = {
+                    activity_id: article.activity.id,
+                    image: article.image
+                }
+                console.log(tools.findRelativePoster(posters, item))
+                if (tools.findRelativePoster(posters, item) == false) {
+                    //console.log("Push")
+                    posters.push(item)
+                }
             })
-          },
-          complete: function() {
-            count++;
-            //console.log(count)
-            if (count === 42) {
-              that.setDateToActivities();
-              that.setData({
-                stages: that.data.stages.reverse()
-              })
-              that.getPostUrl();
-              that.getBriefInformation();
-              that.getCurrentTab();
+        }
+        this.setData({
+            posters
+        })
+        console.log(posters);
+    },
+    formatDateForarticles: function() {
+        let articlesInOneMonth = this.data.articlesInOneMonth;
+        articlesInOneMonth.map((articlesItem) => {
+            articlesItem.articlesInOneDay.map((article) => {
+                article.starttime = tools.formateDateToRegularForm(article.start_time)
+                article.endtime = tools.formateDateToRegularForm(article.end_time)
+            })
+        })
+        this.setData({
+            articlesInOneMonth
+        })
+    },
+    setDateToActivities: function() {
+        let articlesInOneMonth = this.data.articlesInOneMonth;
+        //console.log(stages)
+        let month = (new Date()).getMonth() + 1;
+        var newArticles = articlesInOneMonth.map((articles, index) => {
+            let newObject = {}
+            newObject.articlesInOneDay = articles.map((activity) => {
+                return activity;
+            });
+            newObject.date = month + "月" + (index + 1) + "日";
+            return newObject;
+        })
+        this.setData({
+                articlesInOneMonth: newArticles
+            })
+            //console.log(this.data.stages)
+    },
+    setDateFormat: function() {
+        Date.prototype.format = function() {
+            let year = this.getFullYear();
+            let month = this.getMonth() + 1;
+            let day = this.getDate();
+            month = (month >= 10) ? month.toString() : "0" + month
+            day = (day >= 10) ? day.toString() : "0" + day
+            let str = year + "-" + month + "-" + day;
+            return str.substr(2);
+        }
+    },
+    changeCurrentTap: function(e) {
+        var index = e.currentTarget.dataset.index;
+        this.setData({
+            currentTab: index
+        })
+    },
+    rollToWebview: function() {
+        console.log("roll")
+        wx.navigateTo({
+            url: './articles_webview/index',
+        })
+    },
+    /**
+     * 获取当前位置
+     */
+    getLocation: function() {
+        var that = this
+        var locationString = ""
+        wx.getLocation({
+            type: 'wgs84',
+            success: function(res) {
+                let location = {
+                    latitude: res.latitude,
+                    longitude: res.longitude,
+                    address: "中山大学"
+                }
+                that.setData({
+                    location
+                })
+                locationString = res.latitude + "," + res.longitude
+                that.requestAddress(locationString)
             }
-            //console.log(that.data.stages)
-          }
         })
-        return stage;
-      })
-      return articlesInOneDay;
-    })
-  },
-  // 跳转到活动主页
-  getPostUrl: function() {
-    let stages = this.data.stages;
-    let posters =[]
-    for (let i in stages) {
-      let stagesItem = stages[i]
-      if (stagesItem.stagesInOneDay.length > 0) {
-        let item = {
-          activity_id: stagesItem.stagesInOneDay[0].activity.id,
-          image: stagesItem.stagesInOneDay[0].image
-        }
-        posters.push(item)
-      }
-    }
-    this.setData({
-      posters
-    })
-    console.log(posters);
-  },
-  // 跳转到活动主页
-  getBriefInformation: function() {
-    let stages = this.data.stages;
-    let shortIntroduction = []
-    for (let i in stages) {
-      let stagesItem = stages[i];
-      if (stagesItem.stagesInOneDay.length > 0) {
-        let item = {
-          short_name: stagesItem.stagesInOneDay[0].activity.short_name,
-          activity_id: stagesItem.stagesInOneDay[0].activity.id,
-          color: tools.getHighSColor()
-        }
-        shortIntroduction.push(item)
-      }
-    }
-    this.setData({
-      shortIntroduction
-    })
-    //console.log(shortIntroduction);
-  },
-  setDateToActivities: function() {
-    let stages = this.data.stages;
-    //console.log(stages)
-    let month = (new Date()).getMonth()+1;
-    var activityInOneMonth = stages.map((activities, index) => {
-      let newObject = {}
-      newObject.stagesInOneDay = activities.map((activity) => {
-        return activity;
-      });
-      newObject.date = month+"月"+(index+1)+"日";
-      return newObject;
-    })
-    this.setData({
-      stages: activityInOneMonth
-    })
-    //console.log(this.data.stages)
-  },
-  setDateFormat: function() {
-    Date.prototype.format = function() {
-      let year = this.getFullYear();
-      let month = this.getMonth()+1;
-      let day = this.getDate();
-      month = (month>=10) ? month.toString() : "0" + month
-      day = (day>= 10) ? day.toString() : "0" + day
-      let str = year + "-" + month + "-" + day;
-      return str.substr(2);
-    }
-  },
-  getCurrentTab: function() {
-    let stages = this.data.stages;
-    let day = (new Date()).getDate();
-    let index = stages.length - day-3;
-    this.setData({
-      currentTab: index
-    })
-  },
-  /**
-   * 获取当前位置
-   */
-  getLocation: function() {
-    var that = this
-    var locationString = ""
-    wx.getLocation({
-      type: 'wgs84',
-      success: function(res) {
-        let location = {
-          latitude: res.latitude,
-          longitude: res.longitude,
-          address: "中山大学"
-        }
-        that.setData({
-          location
+    },
+    showSearchBar: function(e) {
+        console.log(e)
+    },
+    /**
+     * 根据经度纬度获取当前位置的中文描述
+     * @param {string} str 包含经度 纬度的字符串
+     */
+    requestAddress: function(str) {
+        var that = this
+        wx.request({
+            url: "https://apis.map.qq.com/ws/geocoder/v1/",
+            data: {
+                "key": "6UVBZ-LUKLU-3ZRV7-BUDEO-SZJCZ-CQBN4",
+                "location": str
+            },
+            methon: "GET",
+            success: function(res) {
+                let location = that.data.location
+                location.address = res.data.result.address_component.city
+                that.setData({
+                    location
+                })
+                console.log("请求地址为：" + that.data.location.address)
+            },
         })
-        locationString = res.latitude + "," + res.longitude
-        that.requestAddress(locationString)
-      }
-    })
-  },
-  showSearchBar: function(e) {
-    console.log(e)
-  },
-  /**
-   * 根据经度纬度获取当前位置的中文描述
-   * @param {string} str 包含经度 纬度的字符串
-   */
-  requestAddress: function(str) {
-    var that = this
-    wx.request({
-      url: "https://apis.map.qq.com/ws/geocoder/v1/",
-      data: {
-        "key": "6UVBZ-LUKLU-3ZRV7-BUDEO-SZJCZ-CQBN4",
-        "location": str
-      },
-      methon: "GET",
-      success: function(res) {
-        let location = that.data.location
-        location.address = res.data.result.address_component.city
-        that.setData({
-          location
+    },
+    /**
+     * 得到搜索框输入
+     * @param {object} e 触发事件
+     */
+    getSearchInput: function(e) {
+        this.setData({
+            searchKey: e.detail.value
         })
-        console.log("请求地址为：" + that.data.location.address)
-      },
-    })
-  },
-  /**
-   * 得到搜索框输入
-   * @param {object} e 触发事件
-   */
-  getSearchInput: function(e) {
-    this.setData({
-      searchKey: e.detail.value
-    })
-  },
-  /**
-   * 把对应的推送设为已浏览
-   * @param {object} e 触发事件
-   */
-  makeVisited: function (e) {
-    var index = e.currentTarget.dataset.index
-    this.data.visited[index] = 1
-    console.log(index)
-    this.setData({
-      visited: this.data.visited
-    })
-  },
-  navigateToActivityDetail: function (e) {
-    wx.navigateTo({
-      url: 'articles_webview/index' // will be some other data
-    })
-  }
+    },
+    /**
+     * 把对应的推送设为已浏览
+     * @param {object} e 触发事件
+     */
+    makeVisited: function(e) {
+        var index = e.currentTarget.dataset.index
+        this.data.visited[index] = 1
+        console.log(index)
+        this.setData({
+            visited: this.data.visited
+        })
+    },
+    navigateToActivityDetail: function(e) {
+        wx.navigateTo({
+            url: 'articles_webview/index' // will be some other data
+        })
+    }
 })
 
-const ARTICLES_SAMPLE = [
-  {
-    id: 0,
-    date: "2月7号",
-    article: [
-      {
+const ARTICLES_SAMPLE = [{
         id: 0,
-        image: "https://i.loli.net/2018/02/28/5a95a4d037347.png",
-        name: "中山大学团委活动",
-        location: "假草操场",
-        mark: [1,1,0],
-        content: "1758舞蹈会",
-        activity_id: 12345
-      }
-    ]
-  },
-  {
-    id: 1,
-    date: "2月8号",
-    article: [
-      {
+        date: "2月7号",
+        article: [{
+            id: 0,
+            image: "https://i.loli.net/2018/02/28/5a95a4d037347.png",
+            name: "中山大学团委活动",
+            location: "假草操场",
+            mark: [1, 1, 0],
+            content: "1758舞蹈会",
+            activity_id: 12345
+        }]
+    },
+    {
         id: 1,
-        image: "https://i.loli.net/2018/02/28/5a95a4d064f71.png",
-        name: "数据科学与计算机学院学长团",
-        location: "三饭小广场",
-        mark: [0, 1, 0],
-        content: "学长团",
-        activity_id: 12345
-      },
-    ]
-  },
-  {
-    id: 2,
-    date: "2月9号",
-    article: [
-      {
+        date: "2月8号",
+        article: [{
+            id: 1,
+            image: "https://i.loli.net/2018/02/28/5a95a4d064f71.png",
+            name: "数据科学与计算机学院学长团",
+            location: "三饭小广场",
+            mark: [0, 1, 0],
+            content: "学长团",
+            activity_id: 12345
+        }, ]
+    },
+    {
         id: 2,
-        image: "https://i.loli.net/2018/02/28/5a95a4d088fd3.png",
-        name: "维纳斯歌手大赛",
-        location: "中大东校区",
-        mark: [0, 1, 1],
-        content: "维纳斯歌手比赛",
-        activity_id: 12345
-      },
-    ]
-  },
-  {
-    id: 3,
-    date: "2月10号",
-    article: [
-      {
+        date: "2月9号",
+        article: [{
+            id: 2,
+            image: "https://i.loli.net/2018/02/28/5a95a4d088fd3.png",
+            name: "维纳斯歌手大赛",
+            location: "中大东校区",
+            mark: [0, 1, 1],
+            content: "维纳斯歌手比赛",
+            activity_id: 12345
+        }, ]
+    },
+    {
         id: 3,
-        image: "https://i.loli.net/2018/02/28/5a95a4d09f6ec.png",
-        name: "暑假三下乡",
-        location: "广州",
-        mark: [0, 1],
-        content: "三下乡义教",
-        activity_id: 12345
-      },
-    ]
-  }
+        date: "2月10号",
+        article: [{
+            id: 3,
+            image: "https://i.loli.net/2018/02/28/5a95a4d09f6ec.png",
+            name: "暑假三下乡",
+            location: "广州",
+            mark: [0, 1],
+            content: "三下乡义教",
+            activity_id: 12345
+        }, ]
+    }
 ]
 
 // wxcf38b0daff83a060
--- wxapp/pages/activities/tools.js
+++ wxapp/pages/activities/tools.js
@@ -4,12 +4,12 @@
  * @return {String} color
  */
 const getRandomColor = () => {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 /**
@@ -18,15 +18,15 @@ const getRandomColor = () => {
  * @return {String} color
  */
 const getHighSColor = () => {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  do {
-    color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-  } while (!colorHasHighS(color))
-  return color;
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    do {
+        color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+    } while (!colorHasHighS(color))
+    return color;
 }
 
 /**
@@ -35,61 +35,92 @@ const getHighSColor = () => {
  * @return {Bool}
  */
 const colorHasHighS = (colorInHex) => {
-  let R = parseInt(colorInHex.substr(1, 2), 16);
-  let G = parseInt(colorInHex.substr(3, 2), 16);
-  let B = parseInt(colorInHex.substr(5, 2), 16);
-  let max = Math.max(R, G, B);
-  let min = Math.min(R, G, B);
-  var S = ((max - min) / max);
-  return (S > 0.7) ? true : false;
+    let R = parseInt(colorInHex.substr(1, 2), 16);
+    let G = parseInt(colorInHex.substr(3, 2), 16);
+    let B = parseInt(colorInHex.substr(5, 2), 16);
+    let max = Math.max(R, G, B);
+    let min = Math.min(R, G, B);
+    var S = ((max - min) / max);
+    return (S > 0.7) ? true : false;
 }
 
+/**
+ * 
+ * @param {http url} url
+ * convert http url to https 
+ */
 const formatUrl = function(url) {
-  try {
-    if (!url) {
-      return "null";
+    try {
+        if (!url) {
+            return "null";
+        }
+        if (url[0] === "\"") {
+            url = url.slice(1)
+        }
+        if (url[url.length - 1] === "\"")
+            url = url.slice(0, url.length - 1)
+        if (url.search("https") === -1) {
+            url = url.slice(4);
+            url = "https" + url;
+        }
+        return url;
+    } catch (e) {
+        console.log(e)
     }
-    if (url[0] === "\"") {
-      url = url.slice(1)
-    }
-    if (url[url.length - 1] === "\"")
-      url = url.slice(0, url.length-1)
-    if (url.search("https") === -1) {
-      url = url.slice(4);
-      url = "https" + url;
-    }
-    return url;
-  }
-  catch(e) {
-    console.log(e)
-  }
 }
 
+/**
+ * 
+ * @param {HTML DATA} data 
+ * @param {HTML ELEMENTS NAME} variable 
+ * get Wechat articles title and image
+ */
 const parseHTML = function(data, variable) {
-  try {
-    let regStr = `${variable}.*`
-    let pattern = RegExp(regStr);
-    let formatPattern = RegExp("\".*\"");
-    let target = pattern.exec(data);
-    if (target === null) {
-      console.log(variable + " missing ")
-      target = (variable === "msg_title" ? this.data.defaultTitle : this.data.defaultImage);
+    try {
+        let regStr = `${variable}.*`
+        let pattern = RegExp(regStr);
+        let formatPattern = RegExp("\".*\"");
+        let target = pattern.exec(data);
+        if (target === null) {
+            console.log(variable + " missing ")
+            target = (variable === "msg_title" ? this.data.defaultTitle : this.data.defaultImage);
+        } else {
+            target = formatPattern.exec(target)[0];
+            target = target.slice(1, target.length - 1);
+            target = (target === null ? (variable === "msg_title" ? this.data.defaultTitle : this.data.defaultImage) : target);
+        }
+        return target;
+    } catch (e) {
+        console.log(e)
     }
-    else {
-      target = formatPattern.exec(target)[0];
-      target = target.slice(1,target.length-1);
-      target = (target === null ? (variable === "msg_title" ? this.data.defaultTitle : this.data.defaultImage) : target);
-    }
-    return target;
-  }
-  catch(e) {
-    console.log(e)
-  }
+}
+
+const findRelativePoster = (posters, item) => {
+    var flag = false;
+    posters.forEach((poster) => {
+        if (poster.activity_id === item.activity_id) {
+            flag = true;
+        }
+    })
+    return flag;
+}
+
+/**
+ * 
+ * @param {date str} str
+ * format date to this form
+ * XX月XX日 
+ */
+const formateDateToRegularForm = (str) => {
+    var date = new Date(str);
+    return (date.getMonth() + 1) + "月" + date.getDate() + "日"
 }
 
 module.exports = {
-  getRandomColor,
-  getHighSColor,
-  formatUrl,
-  parseHTML
+    getRandomColor,
+    getHighSColor,
+    formatUrl,
+    parseHTML,
+    findRelativePoster,
+    formateDateToRegularForm
 }
