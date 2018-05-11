c4c5c95bd6aeed675f554898d137f92e537ec60f
unknown <zoro68.zhang@gmail.com>
2018-05-11 10:30:38 +0800

    首页 推文 活动主页 的跳转

--- wxapp/pages/activities/activity_detail/defaultData.js
+++ wxapp/pages/activities/activity_detail/defaultData.js
@@ -1,3 +1,28 @@
-const activity = [
+const activity = {
+    "id": -1,
+    "short_name": "这里是活动主页",
+    "name": "该活动尚未录入信息",
+    "description": "你怎么可以忘记录入信息",
+    "category": "",
+    "poster_url": "",
+    "logo_url": "",
+    "wechat_url": "https://mp.weixin.qq.com/s/MMHC81qw3jpvDlA3lp6e2g",
+    "sports_medals": "无",
+    "public_service_hours": "无",
+    "prize": "无",
+    "other_prize": "无",
+    "organization_id": 100160,
+    "stages": [{
+        "id": 6,
+        "stage_num": 1,
+        "start_time": "2018-04-04T00:00:00+08:00",
+        "end_time": "2018-04-09T23:59:59+08:00",
+        "location": "中大图蜂管理后台",
+        "content": "快去录入信息",
+        "activity_id": 2
+    }]
+}
 
-]
+module.exports = {
+    activity
+}
