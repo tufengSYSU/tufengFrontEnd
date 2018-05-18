63bacfd6d1ec8faf3be3405cfa74126065db19bb
Tendernesszh <13719345192@163.com>
2018-05-18 23:36:39 +0800

    完成启动报名，不过与后端对接部分还没完全写好

--- wxapp/pages/square/enroll/index.js
+++ wxapp/pages/square/enroll/index.js
@@ -6,7 +6,7 @@ Page({
         endTime: '00:00',
         startDate: '2018-01-01',
         endDate: '2018-01-01',
-
+        stages: [],
     },
 
     onLoad: function(option) {
@@ -31,14 +31,47 @@ Page({
                 var data = res.data.data;
                 console.log(data)
                 var list = [];
+                var idList = [];
                 for(var i in data) {
                     list.push(data[i].activity.name)
+                    //console.log(data[i].activity.id)
+                    idList.push(data[i].activity.id)
                 }
                 that.setData({
                     list
                 })
+                that.setData({
+                    idList
+                })
             },
            
+        })
+    },
+    deleteStage: function(e) {
+        var deleteContent = e.currentTarget.dataset.value;
+        var temp = this.data.stages;
+        for(var i in temp) {
+            if(temp[i].content == deleteContent) {
+                temp.splice(i,1);
+                break;
+            }
+        }
+        //console.log(temp)
+        this.setData({
+            stages: temp,
+        })
+    },
+
+    inputLocal: function(e) {
+        this.setData({
+            Location: e.detail.value,
+        })
+    },
+  
+    inputType: function(e) {
+        this.setData({
+        
+            Type: e.detail.value,
         })
     },
     startDatePickerSelected: function(e) {
@@ -73,8 +106,108 @@ Page({
         })
     },
     
+    addStage: function() {
+        var flag = true;
+        var successFlag = true;
+        var tmpStage = this.data.stages;
+        var tempStage = A_STAGE_SAMPLE;
+        tempStage.activity_id = this.data.idList[this.data.casIndex];
+        tempStage.location = this.data.Location;
+        tempStage.start_time = this.data.startDate+this.data.startTime;
+        tempStage.end_time = this.data.endDate+this.data.endTime;
+        tempStage.content = this.data.Type;
+        //console.log(tempStage)
+
+        if(tempStage.location == undefined) {
+            flag = false;
+            console.log(flag)
+            wx.showToast({
+              title: '请输入报名地点',
+              icon: "loading",
+              duration: 1000,
+              mask: true
+            })
+            return;
+        }
+        if(tempStage.content == undefined) {
+            flag = false;
+            wx.showToast({
+              title: '请输入报名类型',
+              icon: "loading",
+              duration: 1000,
+              mask: true
+            })
+            return;
+        }
+
+        if(flag == true) {
+            var ID = this.data.idList[this.data.casIndex];
+            wx.request({
+                url: `https://ancestree.site/api/activities/${ID}/activity_stages`,
+                header: {
+                  "content-Type": "application/json"
+                },
+                data: {
+                    tempStage
+                },
+                method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+                // header: {}, // 设置请求的 header
+                complete: function(res) {
+                    if(res == null || res.data == null) {
+                      console.error('网络请求失败')
+                      successFlag = false;
+                      return;
+                    } else {
+                      console.log("网络请求成功")
+                      //console.log(res.data.data)
+                      successFlag = true;
+                      wx.showToast({
+                        title: '发布报名并添加活动阶段成功',
+                        icon: 'success',
+                        duration: 1000,
+                        mask: true
+                      })
+    
+                    }
+                }
+               
+            })
+        }
+
+        if(successFlag == true && flag == true) {
+            tmpStage.push(tempStage)
+            //console.log(tempStage)
+            this.setData({
+                stages: tmpStage,
+                startTime: '00:00',
+                endTime: '00:00',
+                startDate: '2018-01-01',
+                endDate: '2018-01-01',
+                Location: "",
+                Type: "",
+                casIndex: 0,
+            })
+            console.log(this.data.stages)
+
+        }
+        //var that = this
+    },
 
 })
+
+const A_STAGE_SAMPLE = {
+    id: null,
+    stage_num: null,
+    start_time: "",
+    end_time: "",
+    location: "",
+    content: "",
+    activity_id: 17,
+}
+const STAGE_SAMPLE = [
+    
+]
+
 const ORGANIZATION_SAMPLE = {
     id: "123",
     background: "https://i.loli.net/2018/03/13/5aa7c6477fcdd.png",
